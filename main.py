fa7adb1dc574d0fd4e950d3b52cc47b0b8f25c30
not-my-circus <hollis.mccray@gmail.com>
2025-03-11 12:10:49 -0700

    Moved maze class to separate file

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from graphics import Window, Line, Point, Cell
+from maze import Maze
 
 
 def main():
-    win = Window(800, 600)
-    c = Cell(win)
-    c.has_bottom_wall = False
-    c.draw(50, 50, 100, 100)
-    c2 = Cell(win)
-    c2.has_top_wall = False
-    c2.draw(50, 100, 100, 150)
-    c.draw_move(c2, True)
 
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
+
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
     win.wait_for_close()
 
 main()
